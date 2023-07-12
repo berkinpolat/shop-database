7416c420899956c43c869e8ddf9f7ca334c0b33b
Berkin Polat <48439107+berkinpolat@users.noreply.github.com>
2023-07-12 18:05:24 +0300

    Project Reorder

--- Views.sql
+++ Views.sql
@@ -1,13 +1,13 @@
--- Ýstanbuldan verilen sipraiþlerin ortalama tutarý
+-- Average cost of orders from Istanbul
 CREATE VIEW avgOrderPrice_Istanbul
 as
 select avg(cb.totalPrice) as averageCost
 from Customer c inner join Address a on c.customerID = a.customerID inner join CustomerOrder co on c.customerID=co.customerID
 inner join CustomerProductOfOrder cpo on cpo.orderID=co.orderID inner join CustomerBill cb on cb.orderID=cpo.orderID
-where a.city = 'Ýstanbul'
+where a.city = 'ï¿½stanbul'
 
 
--- En çok satýn alýnan ürün
+-- Most ordered product
 CREATE VIEW mostOrderedProduct
 as
 select TOP 1 p.productID,p.productName , p.price, sum(cpo.amount) as TotalAmount
@@ -16,7 +16,7 @@ group by p.productID,p.productName , p.price
 order by TotalAmount DESC
 
 
---Hangi þehirden kaç sipariþ verilmiþ.
+-- How many orders have been placed from which city.
 CREATE VIEW orderCountByCity
 as
 select a.city , count(*) as totalOrderCity
@@ -25,8 +25,7 @@ inner join CustomerBill cb on cb.orderID = cpo.orderID inner join CustomerShipme
 group by a.city
 
 
-
--- Hangi kategoriden kaç sipariþ verilmiþ.
+-- How many orders have been placed from which category.
 CREATE VIEW orderCountByCategory
 as
 select  c.categoryName  , sum(cpo.amount) as TotalCategorOrderAmont
@@ -35,8 +34,7 @@ inner join CustomerProductOfOrder cpo on cpo.productID = p.productID
 group by c.categoryName
 
 
-
--- En çok para harcayan ilk 3 þirket
+-- Most money spent by the first 3 companies
 CREATE VIEW mostPurchasedCompany
 as
 select top 3  c.companyID , c.companyName, sum(cb.totalPrice) as totalAmountMoney
@@ -44,5 +42,3 @@ from Company c inner join CompanyOrder co on c.companyID=co.companyID inner join
 CompanyProductOfOrder cpo on cpo.orderID=co.orderID inner join CompanyBill cb on cb.orderID = cpo.orderID
 group by  c.companyID , c.companyName
 order by totalAmountMoney DESC
-
-
